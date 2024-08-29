97becbe8ba26ec47932129675d4faa7575d9d9ba
박종건 <ooostus@gmail.com>
2024-08-29 23:08:04 +0900

    Change Directory

---
+++ programmers/level0/조건에맞게수열변환하기1.py
@@ -0,0 +1,8 @@
+"""
+문제 설명
+정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return 하는 solution 함수를 완성해 주세요.
+"""
+
+def solution(arr):
+    answer = [num // 2 if num >= 50 and num % 2 == 0 else num * 2 if num < 50 and num % 2 == 1 else num for num in arr]
+    return answer
