2c828b8aa46bda23c17dbc1b11f424b190704a86
박종건 <ooostus@gmail.com>
2024-09-16 23:53:23 +0900

    가위바위보

---
+++ programmers/level0/가위바위보.py
@@ -0,0 +1,8 @@
+"""
+문제 설명
+가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+"""
+
+def solution(rsp):
+    answer = ''.join(['0' if i == '2' else '5' if i == '0' else '2' for i in rsp])
+    return answer
