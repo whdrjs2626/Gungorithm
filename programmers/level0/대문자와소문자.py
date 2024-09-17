be5afc7aaa139d719d1a2d788bee309aa4db4fe8
박종건 <ooostus@gmail.com>
2024-09-18 01:40:49 +0900

    대문자와소문자

---
+++ programmers/level0/대문자와소문자.py
@@ -0,0 +1,8 @@
+"""
+문제 설명
+문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+"""
+
+def solution(my_string):
+    answer = ''.join([i.lower() if i.isupper() else i.upper() for i in my_string])
+    return answer
