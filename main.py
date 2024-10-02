66fdb707feed4b67317ef92425c809f8802cadad
Jonggun Park <ooostus@gmail.com>
2024-10-02 23:41:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -111,18 +111,18 @@ if __name__ == "__main__":
     import sys
 
 
-    N = int(sys.stdin.readline())
-    t, p, dp = [0 for _ in range(N + 1)], [0 for _ in range(N + 1)], [0 for _ in range(N + 1)]
-    for i in range(1, N + 1):
-        t[i], p[i] = map(int, sys.stdin.readline().split())
-
-    for i in range(1, N + 1):
-        t, p = map(int, sys.stdin.readline().split())
-        dp[i] = max(dp[i - 1], dp[i])
-        if i + t <= N + 1:
-            dp[i + t - 1] = max(dp[i - 1] + p, dp[i + t - 1])
-
-    print(dp[-1])
+    # N = int(sys.stdin.readline())
+    # t, p, dp = [0 for _ in range(N + 1)], [0 for _ in range(N + 1)], [0 for _ in range(N + 1)]
+    # for i in range(1, N + 1):
+    #     t[i], p[i] = map(int, sys.stdin.readline().split())
+    #
+    # for i in range(1, N + 1):
+    #     t, p = map(int, sys.stdin.readline().split())
+    #     dp[i] = max(dp[i - 1], dp[i])
+    #     if i + t <= N + 1:
+    #         dp[i + t - 1] = max(dp[i - 1] + p, dp[i + t - 1])
+    #
+    # print(dp[-1])
 
 
 
