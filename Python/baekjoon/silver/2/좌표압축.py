ca037432ef780229fe133e1deebeed13fee5fec3
Jonggun Park <ooostus@gmail.com>
2024-11-05 01:07:27 +0900

    Create 좌표압축.py

---
+++ Python/baekjoon/silver/2/좌표압축.py
@@ -0,0 +1,28 @@
+"""
+문제
+수직선 위에 N개의 좌표 X1, X2, ..., XN이 있다. 이 좌표에 좌표 압축을 적용하려고 한다.
+
+Xi를 좌표 압축한 결과 X'i의 값은 Xi > Xj를 만족하는 서로 다른 좌표 Xj의 개수와 같아야 한다.
+
+X1, X2, ..., XN에 좌표 압축을 적용한 결과 X'1, X'2, ..., X'N를 출력해보자.
+
+입력
+첫째 줄에 N이 주어진다.
+
+둘째 줄에는 공백 한 칸으로 구분된 X1, X2, ..., XN이 주어진다.
+
+출력
+첫째 줄에 X'1, X'2, ..., X'N을 공백 한 칸으로 구분해서 출력한다.
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+temp_list = list(map(int, sys.stdin.readline().split()))
+
+sorted_list = sorted(list(set(temp_list)))
+
+result_dict = {sorted_list[i] : i for i in range(len(sorted_list))}
+
+for str in temp_list:
+    print(result_dict[str], end = ' ')
