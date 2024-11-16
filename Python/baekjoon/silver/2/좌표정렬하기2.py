ed8ca75ded9b5d48e8a1ec714f374752d95f6d25
Jonggun Park <ooostus@gmail.com>
2024-11-16 23:57:52 +0900

    Create 좌표정렬하기2.py

---
+++ Python/baekjoon/silver/2/좌표정렬하기2.py
@@ -0,0 +1,23 @@
+"""
+문제
+2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+출력
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+list = []
+
+for _ in range(N):
+    list.append(tuple(map(int, sys.stdin.readline().split()))[::-1])
+
+list.sort()
+
+for a in list:
+    print("{0} {1}".format(a[1], a[0]))
