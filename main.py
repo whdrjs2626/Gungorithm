1b1750366f7a36fa2bdacbd69dddfe92b5ca9702
Jonggun Park <ooostus@gmail.com>
2024-12-29 23:35:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -113,18 +113,7 @@ if __name__ == "__main__":
     print(solution(arr1, arr2))
 """
 
-def solution(arr1, arr2):
-    if len(arr1) > len(arr2):
-        return 1
-    elif len(arr1) < len(arr2):
-        return -1
-    else:
-        if sum(arr1) > sum(arr2):
-            return 1
-        elif sum(arr1) < sum(arr2):
-            return -1
-        else:
-            return 0
+
 
 
 # 2 3 2   5 4 3
