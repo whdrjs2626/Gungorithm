38050e55a7238450d76f3d5a74e7162e4b9ba44d
박종건 <ooostus@gmail.com>
2025-01-21 22:45:56 +0900

    문자열을정수로변환하기

---
+++ Python/programmers/level0/문자열을정수로변환하기.py
@@ -0,0 +1,11 @@
+"""
+문제 설명
+숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n_str ≤ 5
+n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
+"""
+
+def solution(n_str):
+    return int(n_str)
