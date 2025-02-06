4ee8adcb146482e8b26b373b3cf283e65ca891b1
박종건 <ooostus@gmail.com>
2025-02-06 17:07:41 +0900

    문자열겹쳐쓰기

---
+++ Python/programmers/level0/문자열겹쳐쓰기.py
@@ -0,0 +1,12 @@
+"""
+문제 설명
+문자열 my_string, overwrite_string과 정수 s가 주어집니다. 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+my_string와 overwrite_string은 숫자와 알파벳으로 이루어져 있습니다.
+1 ≤ overwrite_string의 길이 ≤ my_string의 길이 ≤ 1,000
+0 ≤ s ≤ my_string의 길이 - overwrite_string의 길이
+"""
+
+def solution(my_string, overwrite_string, s):
+    return my_string[:s] + overwrite_string + my_string[len(overwrite_string)+s:]
