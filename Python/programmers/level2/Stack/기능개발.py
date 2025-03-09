6a926d42dc7321276fedd4435efa64b2a47df3b0
박종건 <ooostus@gmail.com>
2025-03-09 17:57:16 +0900

    기능개발

---
+++ Python/programmers/level2/Stack/기능개발.py
@@ -0,0 +1,35 @@
+"""
+문제 설명
+프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+
+또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+
+먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+
+제한 사항
+작업의 개수(progresses, speeds배열의 길이)는 100개 이하입니다.
+작업 진도는 100 미만의 자연수입니다.
+작업 속도는 100 이하의 자연수입니다.
+배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
+"""
+
+import math
+def solution(progresses, speeds):
+    pivot = len(speeds) - 1
+    progresses = progresses[::-1]
+    speeds = speeds[::-1]
+    result = []
+    while progresses:
+        days = math.ceil((100 - progresses[pivot]) / speeds[pivot])
+        job = []
+        l = len(progresses)
+        flag = 1
+        for i in range(l):
+            progresses[l - i - 1] += speeds[l - i - 1] * days
+            if progresses[l - i - 1] >= 100 and flag:
+                job.append(progresses.pop())
+            else:
+                flag = 0
+        pivot -= len(job)
+        result.append(len(job))
+    return result
