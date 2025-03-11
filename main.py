7c4258de88e0f2a2c834fed815a8152f66f008a5
박종건 <ooostus@gmail.com>
2025-03-11 23:56:41 +0900

    같은숫자는싫어

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+"""
+progresses	speeds	return
+[93, 30, 55]	[1, 30, 5]	[2, 1]
+"""
+def solution(routes):
+    for route in routes:
 
-def solution(citations):
-    return sorted(citations)[len(citations)//2]
+    return result
+
+
 
 if __name__ == "__main__":
-    result = solution([3, 0, 6, 1, 5])
+    result = solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]])
     print(result)
 
