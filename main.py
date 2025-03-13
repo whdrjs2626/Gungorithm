060748acacfdfc5995cc13a65a5da32e54258cd8
박종건 <ooostus@gmail.com>
2025-03-13 23:49:14 +0900

    택배상자꺼내기

--- main.py
+++ main.py
@@ -4,31 +4,22 @@ progresses	speeds	return
 """
 import math
 def solution(n, w, num):
-    # li = []
-    # tmp = []
-    # flag = 1
-    # for i in range(1, n + 1):
-    #     tmp.append(i)
-    #     if i % w == 0:
-    #         if flag % 2 == 0:
-    #             tmp.reverse()
-    #         li.append(tmp)
-    #         tmp = []
-    #         flag += 1
-    # li.append(tmp)
-    # li.reverse()
     div = math.ceil(n / w)
-    cur = num // w
+    cur = math.ceil(num / w)
     last = [i for i in range((div - 1) * w + 1, n + 1)]
     if div % 2 == 0:
         last.reverse()
+        last = ([0] * (w - len(last))) + last
     else:
-        a = 1
-    return div - cur
+        last += [0] * (w - len(last))
+    first = [i for i in range((cur - 1) * w + 1, cur * w + 1)]
+    if cur % 2 == 0:
+        first.reverse()
+    return div - cur + 1 if last[first.index(num)] else div - cur
 
 
 
 if __name__ == "__main__":
-    result = solution(22, 6, 8)
+    result = solution(13,3,6)
     print(result)
 
