023694d0e40d45de19dcc37f867753772b76cb4a
박종건 <ooostus@gmail.com>
2025-03-24 17:35:18 +0900

    주사위게임1

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ progresses	speeds	return
 [93, 30, 55]	[1, 30, 5]	[2, 1]
 """
 import math
-def solution(n, w, num):
-    div = math.ceil(n / w)
-    cur = math.ceil(num / w)
-    last = [i for i in range((div - 1) * w + 1, n + 1)]
-    if div % 2 == 0:
-        last.reverse()
-        last = ([0] * (w - len(last))) + last
+def solution(a, b):
+    if (a + b) % 2 == 0:
+        if a % 2 == 0 and b % 2 == 0:
+            return abs(a - b)
+        else:
+            return a**2 + b**2
     else:
-        last += [0] * (w - len(last))
-    first = [i for i in range((cur - 1) * w + 1, cur * w + 1)]
-    if cur % 2 == 0:
-        first.reverse()
-    return div - cur + 1 if last[first.index(num)] else div - cur
+        return 2 * (a + b)
 
 
 
 if __name__ == "__main__":
-    # result = solution(13,3,6)
-    # print(result)
+    result = solution(3, 5)
+    print(result)
 
