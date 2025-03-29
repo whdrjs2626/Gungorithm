d025a4a5e4f7385f3c9877074a98095db9b3ddb7
Jonggun Park <ooostus@gmail.com>
2025-03-30 02:47:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 progresses	speeds	return
 [93, 30, 55]	[1, 30, 5]	[2, 1]
 """
-import math
-def solution(a, b):
-    if (a + b) % 2 == 0:
-        if a % 2 == 0 and b % 2 == 0:
-            return abs(a - b)
-        else:
-            return a**2 + b**2
-    else:
-        return 2 * (a + b)
+# import math
+# def solution(a, b):
+#     if (a + b) % 2 == 0:
+#         if a % 2 == 0 and b % 2 == 0:
+#             return abs(a - b)
+#         else:
+#             return a**2 + b**2
+#     else:
+#         return 2 * (a + b)
 
 
 
