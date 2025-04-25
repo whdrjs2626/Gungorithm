155171bccce9491b87545f1f03683a6ceee86580
Jonggun Park <ooostus@gmail.com>
2025-04-26 00:12:55 +0900

    Create 첫번째로나오는음수.py

---
+++ Python/programmers/level0/첫번째로나오는음수.py
@@ -0,0 +1,14 @@
+"""
+문제 설명
+정수 리스트 num_list가 주어질 때, 첫 번째로 나오는 음수의 인덱스를 return하도록 solution 함수를 완성해주세요. 음수가 없다면 -1을 return합니다.
+
+제한사항
+5 ≤ num_list의 길이 ≤ 100
+-10 ≤ num_list의 원소 ≤ 100
+"""
+
+def solution(num_list):
+    for i, num in enumerate(num_list):
+        if num < 0:
+            return i
+    return -1
