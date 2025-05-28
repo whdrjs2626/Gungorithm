d6809458bbceb6b61bf45753db7333e13625a7f7
Jonggun Park <75626100+whdrjs2626@users.noreply.github.com>
2025-05-28 14:47:56 +0000

    부분문자열이어붙여문자열만들기

---
+++ Python/programmers/level0/부분문자열이어붙여문자열만들기.py
@@ -0,0 +1,9 @@
+"""
+문제 설명
+
+길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다. 각 my_strings의 원소의 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+"""
+
+def solution(my_strings, parts):
+  return ''.join([string[parts[i][0]:parts[i][1] + 1] for i, string in enumerate(my_strings)])
+  
