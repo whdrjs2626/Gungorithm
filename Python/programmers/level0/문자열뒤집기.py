37a273a613ad5fa4281e486269b251b1825e2d1f
Jonggun Park <75626100+whdrjs2626@users.noreply.github.com>
2025-06-12 14:58:05 +0000

    문자열뒤집기

---
+++ Python/programmers/level0/문자열뒤집기.py
@@ -0,0 +1,13 @@
+"""
+문제 설명
+
+문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+제한사항
+my_string은 숫자와 알파벳으로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+0 ≤ s ≤ e < my_string의 길이
+"""
+
+def solution(my_string, s, e):
+  return my_string[:s] + my_string[s:e:-1] + my_string[e:]
+  
