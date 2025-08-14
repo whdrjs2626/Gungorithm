030ff12f95714e0052c1588fbf445d902826ffdc
박종건 <ooostus@gmail.com>
2025-08-14 17:50:58 +0900

    배열만들기3 다른 방식 추가

--- Python/programmers/level0/배열만들기3.py
+++ Python/programmers/level0/배열만들기3.py
@@ -2,3 +2,9 @@ def solution(arr, intervals):
   s1, e1 = intervals[0]
   s2, e2 = intervals[1]
   return arr[s1:e1+1] + arr[s2:e2+1]
+
+
+import itertools
+def solution2(arr, intervals):
+    return list(itertools.chain.from_iterable([arr[inter[0]:inter[1]+1] for inter in intervals]))
+
